e287e9432b9f52ddd7d5c7c13f9791283d69dfc7
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2022-11-18 16:23:25 +0100

    Remove None default values.

--- flows/flow_parameter_types/ExperimentConfig.py
+++ flows/flow_parameter_types/ExperimentConfig.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel
 
 class ExperimentConfig(BaseModel):
     exp_path: str = "/path/to/experiment.yaml"
-    sample_name: str = None
-    acquisition: str = None
-    start_section_num: int = None
-    end_section_num: int = None
-    tile_grid_num: int = None
+    sample_name: str = "Sample"
+    acquisition: str = "run"
+    start_section_num: int = 1
+    end_section_num: int = 10
+    tile_grid_num: int = 1
