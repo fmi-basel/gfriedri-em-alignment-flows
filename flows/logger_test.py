b48585fad86690765a696db17bb7b826d5f36fbc
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2022-11-21 14:29:50 +0100

    Remove work.

--- flows/logger_test.py
+++ flows/logger_test.py
@@ -1,9 +1,5 @@
-from typing import Literal
-
-import numpy as np
 from prefect import flow, get_run_logger, task
 from prefect_dask import DaskTaskRunner
-from pydantic import BaseModel
 
 runner = DaskTaskRunner(
     cluster_class="dask_jobqueue.SLURMCluster",
@@ -36,26 +32,17 @@ runner = DaskTaskRunner(
 def a_task(i):
     logger = get_run_logger()
     logger.info(f"I am logging from a task. {i}")
-    img = np.random.rand(1000, 1000)
-    for y in range(1000):
-        for x in range(1000):
-            img[y, x] = img[x, y]
 
     logger.info("Done.")
     # sleep(5)
     # os.system("sleep 5")
 
 
-class FMI_User(BaseModel):
-    group: Literal["gmicro", "gfriedri", "gchao"]
-    user: str
-
-
 @flow(
     name="Logger Test",
     task_runner=runner,
 )
-def flow(group_user: FMI_User):
+def flow():
     logger = get_run_logger()
     logger.info("Flow starts!")
 
