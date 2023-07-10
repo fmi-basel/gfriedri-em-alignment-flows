48548616ae9957b9f3940ca46acf26d0a04d5537
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2023-07-10 14:47:45 +0200

    Fix clip_limit default value.

--- 01_tile-stitching/build_config.py
+++ 01_tile-stitching/build_config.py
@@ -255,7 +255,7 @@ def get_warp_config():
         clip_limit = float(
             questionary.text(
                 "warp_config.clip_limit:",
-                default="1024",
+                default="0.01",
                 validate=lambda v: v.replace(".", "").isdigit(),
             ).ask()
         )
