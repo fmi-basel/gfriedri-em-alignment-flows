0c838e91c17c3339cb94d7c81124d740f5742d58
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2023-08-02 17:13:52 +0200

    Add flow to create coarse-aligned stack.

--- 02_coarse-section-alignment/s02_create_coarse_aligned_stack.py
+++ 02_coarse-section-alignment/s02_create_coarse_aligned_stack.py
@@ -28,6 +28,74 @@ def get_padding_per_section(shifts):
     return np.concatenate([np.array([[0, 0]]), cumulated_padding], 0)
 
 
+def write_section(
+    section_dir: str,
+    padding,
+    start_section: int,
+    yx_start: tuple[int, int],
+    yx_size: tuple[int, int],
+    bin: int,
+    zarr_root: zarr.Group,
+):
+    scaler = Scaler(max_layer=4)
+    sec_id = int(basename(section_dir).split("_")[0][1:])
+    output_index = sec_id - start_section
+    current = zarr.Group(parse_url(section_dir).store)
+    y_pad, x_pad = padding
+    data = block_reduce(
+        current[0][
+            yx_start[0] : yx_start[0] + yx_size[0] - y_pad,
+            yx_start[1] : yx_start[1] + yx_size[1] - x_pad,
+        ],
+        block_size=bin,
+        func=np.mean,
+    ).astype(np.uint8)
+
+    for level in range(scaler.max_layer + 1):
+        y_start = y_pad // bin // (scaler.downscale**level)
+        y_end = y_start + data.shape[0]
+        x_start = x_pad // bin // (scaler.downscale**level)
+        x_end = x_start + data.shape[1]
+        zarr_root[level][
+            output_index,
+            y_start:y_end,
+            x_start:x_end,
+        ] = data
+        data = scaler.resize_image(data)
+
+
+def create_zarr(
+    output_dir: str,
+    volume_name: str,
+    start_section: int,
+    end_section: int,
+    yx_size: tuple[int, int],
+    bin: int,
+):
+    target_dir = join(output_dir, volume_name)
+    store = parse_url(target_dir, mode="w").store
+    zarr_root = zarr.group(store=store)
+
+    chunks = (1, 2744, 2744)
+    write_image(
+        image=np.zeros(
+            (end_section - start_section, yx_size[0] // bin, yx_size[1] // bin),
+            dtype=np.uint8,
+        ),
+        group=zarr_root,
+        axes="zyx",
+        scaler=Scaler(max_layer=4),
+        storage_options=dict(
+            chunks=chunks,
+            compressor=Blosc(cname="zstd", clevel=3, shuffle=Blosc.SHUFFLE),
+            overwrite=True,
+            write_empty_chunks=False,
+        ),
+    )
+
+    return target_dir
+
+
 def main(
     stitched_section_dir: str,
     output_dir: str,
@@ -48,44 +116,30 @@ def main(
 
     section_paddings = get_padding_per_section(shifts)
 
-    store = parse_url(join(output_dir, volume_name), mode="w").store
-    zarr_root = zarr.group(store=store)
-
-    write_image(
-        image=np.zeros(
-            (end_section - start_section, yx_size[0] // bin, yx_size[1] // bin),
-            dtype=np.uint8,
-        ),
-        group=zarr_root,
-        axes="zyx",
-        scaler=Scaler(max_layer=0),
-        storage_options=dict(
-            chunks=(1, 2744, 2744),
-            compressor=Blosc(cname="zstd", clevel=3, shuffle=Blosc.SHUFFLE),
-            overwrite=True,
-            write_empty_chunks=False,
-        ),
+    zarr_path = create_zarr(
+        output_dir=output_dir,
+        volume_name=volume_name,
+        start_section=start_section,
+        end_section=end_section,
+        yx_size=yx_size,
+        bin=bin,
     )
+
+    store = parse_url(zarr_path, mode="w").store
+    zarr_root = zarr.group(store=store)
 
     for i in tqdm(range(len(section_dirs) - 1)):
         sec_id = int(basename(section_dirs[i]).split("_")[0][1:])
         if start_section <= sec_id < end_section:
-            output_index = sec_id - start_section
-            current = zarr.Group(parse_url(section_dirs[i]).store)
-            y_pad, x_pad = section_paddings[i]
-            data = block_reduce(
-                current[0][
-                    yx_start[0] : yx_start[0] + yx_size[0] - y_pad,
-                    yx_start[1] : yx_start[1] + yx_size[1] - x_pad,
-                ],
-                block_size=bin,
-                func=np.mean,
-            ).astype(np.uint8)
-            zarr_root[0][
-                output_index,
-                y_pad // bin : y_pad // bin + data.shape[0],
-                x_pad // bin : x_pad // bin + data.shape[1],
-            ] = data
+            write_section(
+                section_dir=section_dirs[i],
+                padding=section_paddings[i],
+                start_section=start_section,
+                yx_start=yx_start,
+                yx_size=yx_size,
+                bin=bin,
+                zarr_root=zarr_root,
+            )
 
 
 if __name__ == "__main__":
