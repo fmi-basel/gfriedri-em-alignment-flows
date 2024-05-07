eed0bccd0c155ce5c2b7c5ff6993b3629c825a8e
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2024-05-07 08:40:48 +0200

    wip

--- s02_coarse-section-alignment/slurm-job_run-workflow.sh
+++ s02_coarse-section-alignment/slurm-job_run-workflow.sh
@@ -36,7 +36,7 @@ root_dir="$(pwd)/../.."
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('../../infrastructure/miniforge3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('$root_dir/infrastructure/miniforge3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
