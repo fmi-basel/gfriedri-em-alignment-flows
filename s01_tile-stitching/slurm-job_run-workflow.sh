01d423fe957cbe1b74ce2d3962bc1b513b45ba7a
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2024-05-07 09:09:40 +0200

    wip

--- s01_tile-stitching/slurm-job_run-workflow.sh
+++ s01_tile-stitching/slurm-job_run-workflow.sh
@@ -36,8 +36,8 @@ root_dir="$(pwd)/../.."
 
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'mamba init' !!
-export MAMBA_EXE='$root_dir/infrastructure/micromamba/bin/micromamba';
-export MAMBA_ROOT_PREFIX='$root_dir/infrastructure/micromamba';
+export MAMBA_EXE="$root_dir/infrastructure/micromamba/bin/micromamba";
+export MAMBA_ROOT_PREFIX="$root_dir/infrastructure/micromamba";
 __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__mamba_setup"
