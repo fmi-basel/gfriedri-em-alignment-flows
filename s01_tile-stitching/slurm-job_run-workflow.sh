e26d527575611dcbf2277ba9a1475a4b012ccf25
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2024-05-07 10:48:07 +0200

    wip

--- s01_tile-stitching/slurm-job_run-workflow.sh
+++ s01_tile-stitching/slurm-job_run-workflow.sh
@@ -47,6 +47,7 @@ fi
 unset __mamba_setup
 # <<< mamba initialize <<<
 alias mamba=micromamba
+alias conda=micromamba
 micromamba run -p "$root_dir/infrastructure/envs/nf" nextflow run "$root_dir/s01_tile-stitching/workflow.nf" --config $(pwd)/tile_stitching_config.yaml -profile slurm -with-report -resume -disable-jobs-cancellation
 
 ### END OF PUT YOUR CODE IN THIS SECTION
