d744c65b0dae0975d9c30bfb90af40d06da92bab
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2024-05-13 09:40:24 +0200

    Merge config.

--- s02_coarse-section-alignment/build_config.py
+++ s02_coarse-section-alignment/build_config.py
@@ -3,79 +3,44 @@ import yaml
 
 
 def build_config():
-    config_option = questionary.select(
-        "Build config for:",
-        choices=["coarse-align", "coarse-stack"],
-        default="coarse-align",
+    stitched_sections_dir = questionary.path(
+        "Path to the stitched sections " "directory:"
     ).ask()
 
-    if config_option == "coarse-align":
-        stitched_sections_dir = questionary.path(
-            "Path to the stitched sections " "directory:"
+    start_section = int(
+        questionary.text(
+            "start_section:",
+            default="0",
+            validate=lambda v: v.isdigit(),
         ).ask()
-
-        start_section = int(
-            questionary.text(
-                "start_section:",
-                default="0",
-                validate=lambda v: v.isdigit(),
-            ).ask()
-        )
-        end_section = int(
-            questionary.text(
-                "end_section:",
-                default="10",
-                validate=lambda v: v.isdigit(),
-            ).ask()
-        )
-
-        config = dict(
-            stitched_sections_dir=stitched_sections_dir,
-            start_section=start_section,
-            end_section=end_section,
-        )
-
-        with open("coarse-align.config", "w") as f:
-            yaml.safe_dump(config, f, sort_keys=False)
-    elif config_option == "coarse-stack":
-        stitched_sections_dir = questionary.path(
-            "Path to the stitched sections " "directory:"
+    )
+    end_section = int(
+        questionary.text(
+            "end_section:",
+            default="10",
+            validate=lambda v: v.isdigit(),
         ).ask()
+    )
 
-        start_section = int(
-            questionary.text(
-                "start_section:",
-                default="0",
-                validate=lambda v: v.isdigit(),
-            ).ask()
-        )
-        end_section = int(
-            questionary.text(
-                "end_section:",
-                default="10",
-                validate=lambda v: v.isdigit(),
-            ).ask()
-        )
+    output_dir = questionary.text("Output dir:").ask()
+    volume_name = questionary.text("Volume name:").ask()
+    bin = int(
+        questionary.text(
+            "Bin factor:", default="2", validate=lambda v: v.isdigit()
+        ).ask()
+    )
 
-        output_dir = questionary.text("Output dir:").ask()
-        volume_name = questionary.text("Volume name:").ask()
-        bin = int(
-            questionary.text(
-                "Bin factor:", default="2", validate=lambda v: v.isdigit()
-            ).ask()
-        )
+    config = dict(
+        stitched_sections_dir=stitched_sections_dir,
+        start_section=start_section,
+        end_section=end_section,
+        output_dir=output_dir,
+        volume_name=volume_name,
+        bin=bin,
+    )
 
-        config = dict(
-            stitched_sections_dir=stitched_sections_dir,
-            start_section=start_section,
-            end_section=end_section,
-            output_dir=output_dir,
-            volume_name=volume_name,
-            bin=bin,
-        )
-
-        with open("coarse-stack.config", "w") as f:
-            yaml.safe_dump(config, f, sort_keys=False)
+    with open("coarse-align.config", "w") as f:
+        yaml.safe_dump(config, f, sort_keys=False)
 
 
 if __name__ == "__main__":
